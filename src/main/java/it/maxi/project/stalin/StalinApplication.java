90d6040f191dd0e1afaadf99e765cbe77e89d5c1
msyaskov <ms.yaskov@gmail.com>
2022-12-17 22:32:43 +0300

    Initial

---
+++ src/main/java/it/maxi/project/stalin/StalinApplication.java
@@ -0,0 +1,13 @@
+package it.maxi.project.stalin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StalinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StalinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/maxi/project/stalin/StalinApplicationTests.java
@@ -0,0 +1,13 @@
+package it.maxi.project.stalin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
