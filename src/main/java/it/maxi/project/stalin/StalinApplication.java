c102bdf13dff2dc592cf5485a6a9e8de7c4462ac
msyaskov <ms.yaskov@gmail.com>
2022-12-20 01:26:50 +0300

    gradle groovy

--- src/main/java/it/maxi/project/stalin/StalinApplication.java
+++ src/main/java/it/maxi/project/stalin/StalinApplication.java
@@ -1,7 +1,6 @@
 package it.maxi.project.stalin;
 
 import it.maxi.project.stalin.service.vk.VkService;
-import it.maxi.project.utilita.array.Ints;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +13,6 @@ public class StalinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StalinApplication.class, args);
-        System.out.println(Ints.of(1, 2, 3));
     }
 
     @Bean
