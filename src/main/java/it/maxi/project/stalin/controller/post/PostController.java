5930ce7bbb184e1e7aa1ef79d897bf550fa37850
msyaskov <ms.yaskov@gmail.com>
2022-12-24 21:37:26 +0300

    Added annotation @Controller to PostController
    I forgot to add it earlier

--- src/main/java/it/maxi/project/stalin/controller/post/PostController.java
+++ src/main/java/it/maxi/project/stalin/controller/post/PostController.java
@@ -4,6 +4,7 @@ import it.maxi.project.stalin.model.vk.VkPost;
 import it.maxi.project.stalin.service.vk.VkService;
 import it.maxi.project.stalin.util.Json;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Controller
 public class PostController {
 
     @Autowired
